f47268daf12b236792aaeaf6687d849d39ce2aa4
Andrew Heiss <andrew@andrewheiss.com>
2025-05-01 23:21:04 -0400

    Nicer deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env sh
+
 REMOTE_HOST="ath-cloud"
 REMOTE_DIR="~/sites/stats.andrewheiss.com/public_html/golden-gorilla"
 REMOTE_DEST=$REMOTE_HOST:$REMOTE_DIR
@@ -5,3 +7,14 @@ REMOTE_DEST=$REMOTE_HOST:$REMOTE_DIR
 echo "Uploading new changes to remote server..."
 echo
 rsync -crvP --exclude '*_cache' --delete _book/ $REMOTE_DEST
+
+if [ $? -eq 0 ]; then
+  echo
+  echo "... done!"
+  echo
+  echo "The site is live at https://stats.andrewheiss.com/golden-gorilla/"
+else
+  echo
+  echo "Error!"
+  exit 1
+fi
